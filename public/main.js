25b98fcf59800acfce04ed7935c7e786fc27bb96
JB Bell <tquidca@gmail.com>
2023-03-05 11:59:11 -0800

    Ready to deploy

--- public/main.js
+++ public/main.js
@@ -1,1 +1,32 @@
-console.log('test');
+async function init() {
+    let rustApp = null
+
+    try {
+        rustApp = await import('../pkg')
+    } catch(e) {
+        console.error(e)
+        return;
+    }
+
+    console.log(rustApp)
+
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
+        console.log(input.files[0])
+        let img_data_url = rustApp.grayscale(base64)
+        document.getElementById('new-img').setAttribute(
+            'src', img_data_url
+        )
+    }
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0])
+    })
+}
+
+init()
